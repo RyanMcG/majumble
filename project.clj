e2c92d102394bf640831eb73746156b75994e300
Ryan McGowan <ryan@ryanmcg.com>
2013-07-09 23:42:20 -0700

    Add instaparse and vectorz-clj dependencies.

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [net.mikera/vectorz-clj "0.10.0"]
+                 [instaparse "1.2.0"]]
   :profiles {:dev {:dependencies [[speclj "2.5.0"]]}}
   :plugins [[speclj "2.5.0"]]
   :test-paths ["spec"]
