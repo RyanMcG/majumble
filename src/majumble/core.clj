6ddb7c4f02484b1ff0efa3b564f2126c283c5a90
Ryan McGowan <ryan@ryanmcg.com>
2013-07-10 00:43:04 -0700

    Implement postfix version of source->tree.

--- src/majumble/core.clj
+++ src/majumble/core.clj
@@ -1,18 +1,19 @@
 (ns majumble.core
+  "A postfix, quick and dirty martix manipulation language."
   (:require [instaparse.core :as insta]
             [clojure.core.matrix :as ma]
             [clojure.core.matrix.operators :as mo]))
 
 (ma/set-current-implementation :vectorz)
 
-(def parse
+(def source->tree
   "A function to parse majumble code."
-  (insta/parser "S = VS OE*
-                 <VS> = (V <W>)* V <W?>
+  (insta/parser "S = VS | OE
+                 <VS> = (V <W>)* V
                  <V> = M | NS
                  M = <'['> (NS | (M <W>?)+) <']'>
                  NS = (N <W>)* N <W?>
                  <N> = #'[0-9]+'
-                 OE = O <W> VS
-                 O = '+' | '*' | '/' | '-' | \"'\"
+                 OE = VS <W> O | OE <W> VS <W> O
+                 O = '+' | '*' | '/' | '-' | \"'\" | 'x' | '.'
                  W = #'\\s+'"))
