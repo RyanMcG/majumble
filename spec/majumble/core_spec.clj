291f89d4171b29ced1195d8823da78c3b3e051c7
Ryan McGowan <ryan@ryanmcg.com>
2013-07-09 23:42:39 -0700

    Implement single expression parser and tests.

--- spec/majumble/core_spec.clj
+++ spec/majumble/core_spec.clj
@@ -2,6 +2,18 @@
   (:require [speclj.core :refer :all]
             [majumble.core :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 1 1)))
+(describe "parsing"
+  (it "parses number arguments"
+    (should= [:S [:NS "10" "9"] [:OE [:O "+"] [:NS "8" "7"]]]
+             (parse "10 9 + 8 7")))
+  (it "parses matrix arguments"
+      (should= [:S [:M [:NS "10" "9"]] [:OE [:O "+"] [:M [:NS "8" "7"]]]]
+               (parse "[10 9] + [8 7]")))
+      (it "parses 2-D matrix arguments"
+          (should= [:S
+                    [:M [:M [:NS "1" "2"]] [:M [:NS "3" "4"]]]
+                    [:OE [:O "+"]
+                     [:M [:M [:NS "5" "6"]] [:M [:NS "7" "8"]]]]]
+                   (parse "[[1 2] [3 4]] + [[5 6] [7 8]]"))))
+
+(run-specs)
--- src/majumble/core.clj
+++ src/majumble/core.clj
@@ -1,1 +1,18 @@
-(ns majumble.core)
+(ns majumble.core
+  (:require [instaparse.core :as insta]
+            [clojure.core.matrix :as ma]
+            [clojure.core.matrix.operators :as mo]))
+
+(ma/set-current-implementation :vectorz)
+
+(def parse
+  "A function to parse majumble code."
+  (insta/parser "S = VS OE*
+                 <VS> = (V <W>)* V <W?>
+                 <V> = M | NS
+                 M = <'['> (NS | (M <W>?)+) <']'>
+                 NS = (N <W>)* N <W?>
+                 <N> = #'[0-9]+'
+                 OE = O <W> VS
+                 O = '+' | '*' | '/' | '-' | \"'\"
+                 W = #'\\s+'"))
