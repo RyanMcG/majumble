c46fa0cb92db00d2758bf9cb1e35e00449abbb21
Ryan McGowan <ryan@ryanmcg.com>
2013-07-11 19:50:26 -0700

    Always display expected and actual in should-matrix==.

--- src/speclj/shoulds/matrix.clj
+++ src/speclj/shoulds/matrix.clj
@@ -1,12 +1,22 @@
 (ns speclj.shoulds.matrix
   (:require (speclj [core :refer [-to-s -fail]]
-                    [platform :refer [endl]])
+                    [platform :refer [new-exception endl]])
             [clojure.core.matrix.operators :as mo]))
 
 (defmacro should-matrix==
   [expected-form actual-form]
   `(let [expected# ~expected-form
-         actual# ~actual-form]
-     (if-not (mo/== expected# actual#)
-       (-fail (str "Expected: " (-to-s expected#) endl
-                   "     got: " (-to-s actual#) " (using matrix ==)")))))
+         actual# ~actual-form
+         fail-message# (str "Expected: " (-to-s expected#) endl
+                            "     got: " (-to-s actual#)
+                            " (using matrix ==)")]
+     (try
+       (when-not (mo/== expected# actual#) (-fail fail-message#))
+       (catch java.lang.RuntimeException ex#
+         (let [message# (str fail-message# endl
+                             "Cause: "(.getMessage ex#))
+               ex-stacktrace# (.getStackTrace ex#)]
+           (throw
+             (doto (new-exception message#)
+               (.initCause ex#)
+               (.setStackTrace ex-stacktrace#))))))))
