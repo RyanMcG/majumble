93138daeb7088eb2a4db4a761781366715e4435d
Ryan McGowan <ryan@ryanmcg.com>
2013-07-24 21:34:38 -0700

    Modify specs to fit new AST (no more NS).

--- spec/majumble/core_spec.clj
+++ spec/majumble/core_spec.clj
@@ -5,15 +5,15 @@
 
 (describe "creating parse-trees"
   (it "parses without operators"
-    (should= [:S [:NS "10" "9"]]
+    (should= [:S [:N "10"] [:N "9"]]
              (source->tree "10 9")))
   (it "parses number arguments"
     (should= [:S [:OE
                   [:AS
                    [:OE
-                    [:AS [:NS "10" "9"]]
+                    [:AS [:N "10"] [:N "9"]]
                     [:O "+"]]
-                   [:NS "3"]]
+                   [:N "3"]]
                   [:O "*"]]]
              (source->tree "10 9 + 3 *")))
   (it "parses matrix arguments"
@@ -22,39 +22,39 @@
                [:AS
                 [:OE
                  [:AS
-                  [:M [:NS "10" "9"]] [:M [:NS "8" "7"]]]
+                  [:M [:N "10"] [:N "9"]] [:M [:N "8"] [:N "7"]]]
                  [:O "+"]]
-                [:M [:NS "3" "4"]]]
+                [:M [:N "3"] [:N "4"]]]
                [:O "x"]]]
              (source->tree "[10 9] [8 7] + [3 4] x")))
   (it "parses multi-dimensional matrix arguments"
     (should= [:S
               [:OE
                [:AS
-                [:M [:M [:NS "1" "2"]] [:M [:NS "3" "4"]]]
-                [:M [:M [:NS "5" "6"]] [:M [:NS "7" "8"]]]]
+                [:M [:M [:N "1"] [:N "2"]] [:M [:N "3"] [:N "4"]]]
+                [:M [:M [:N "5"] [:N "6"]] [:M [:N "7"] [:N "8"]]]]
                [:O "+"]]]
              (source->tree "[[1 2] [3 4]] [[5 6] [7 8]] +"))))
 
 (describe "converting parse-trees to an internal representation"
   (it "handles numbers"
     (should= '(10 9 8)
-             (transform [:S [:NS "10" "9" "8"]])))
+             (transform [:S [:N "10"] [:N "9"] [:N "8"]])))
   (it "handles matrices"
     (should-matrix== '([10 9 8] [1 2])
                      (transform [:S
-                                 [:M [:NS "10" "9" "8"]]
-                                 [:M [:NS "1" "2"]]]))
+                                 [:M [:N "10"] [:N "9"] [:N "8"]]
+                                 [:M [:N "1"] [:N "2"]]]))
     (should-matrix== '([[1 2] [3 4]])
                      (transform [:S [:M
-                                     [:M [:NS "1" "2"]]
-                                     [:M [:NS "3" "4"]]]])))
+                                     [:M [:N "1"] [:N "2"]]
+                                     [:M [:N "3"] [:N "4"]]]])))
   (it "handles operators"
     (should= '(3)
-             (transform [:S [:OE [:AS [:NS "1" "2"]] [:O "+"]]]))
+             (transform [:S [:OE [:AS [:N "1"] [:N "2"]] [:O "+"]]]))
     (should-matrix== '([3])
                      (transform [:S [:OE
-                                     [:AS [:M [:NS "1"]] [:M [:NS "2"]]]
+                                     [:AS [:M [:N "1"]] [:M [:N "2"]]]
                                      [:O "+"]]]))))
 
 (describe "parsing source to internal representation"
