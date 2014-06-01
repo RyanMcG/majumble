8dbd142db14f0f86b0ef56e09da9885740cc81d7
Ryan McGowan <ryan@ryanmcg.com>
2014-06-01 13:27:24 -0700

    Upgrade deps and bump version to 0.1.0.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject majumble "0.1.0-SNAPSHOT"
-  :description "A j-like language for matrix manipulation using vectorz-clj."
+(defproject majumble "0.1.0"
+  :description "A postfix language for matrix manipulation using vectorz-clj."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [net.mikera/vectorz-clj "0.10.0"]
-                 [instaparse "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [net.mikera/vectorz-clj "0.22.0"]
+                 [instaparse "1.3.2"]]
   :profiles {:dev {:dependencies [[speclj "2.7.4"]
                                   [speclj.shoulds.matrix "0.1.0-SNAPSHOT"]]}}
   :plugins [[speclj "2.7.4"]]
